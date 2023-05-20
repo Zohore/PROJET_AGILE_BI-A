8770aa09bd941917d3b9a83a0dfc876f4c51c045
hibalb12 <134170823+hibalb12@users.noreply.github.com>
2023-05-20 21:58:29 +0100

    Add files via upload

---
+++ Fonctionnalit‚5_client/client_back/ClientType_form.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+
+class ClientType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('tel')
+            ->add('fax')
+            ->add('email')
+            ->add('adresse')
+            ->add('ice')
+            ->add('echeance')
+            ->add('contratCadreFile', VichFileType::class, [
+                'required' => false,
+                'allow_delete' => true,
+                'label' => 'Contrat Cadre'
+                //   'delete_label' => 'form.label.delete',
+
+            ])
+        ;
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Client'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_client';
+    }
+
+
+}
