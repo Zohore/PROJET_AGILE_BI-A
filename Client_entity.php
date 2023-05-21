90db28c0c07665b057251645af2104a751bb0840
Zohore <77920493+Zohore@users.noreply.github.com>
2023-05-21 16:23:33 +0100

    Add files via upload

---
+++ Client_entity.php
@@ -0,0 +1,649 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+
+/**
+ * Client
+ *
+ * @ORM\Table(name="client")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ClientRepository")
+ * @Vich\Uploadable
+ * @Gedmo\Loggable
+ */
+class Client
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Versioned
+     * @ORM\Column(name="nom", type="string", length=255, nullable=true)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Versioned
+     * @ORM\Column(name="tel", type="string", length=255, nullable=true)
+     */
+    private $tel;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Versioned
+     * @ORM\Column(name="fax", type="string", length=255, nullable=true)
+     */
+    private $fax;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Versioned
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Versioned
+     * @ORM\Column(name="adresse", type="text",nullable=true)
+     */
+    private $adresse;
+    /**
+     * @var string
+     *
+     * @Gedmo\Versioned
+     * @ORM\Column(name="ice", type="string", length=255, nullable=true)
+     */
+    private $ice;
+    /**
+     * @var string
+     *
+     * @Gedmo\Versioned
+     * @ORM\Column(name="echeance", type="string", length=255, nullable=true)
+     */
+    private $echeance;
+
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Mission", mappedBy="client",cascade={"persist", "remove"})
+     */
+    private $missions;
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Projet", mappedBy="client",cascade={"persist", "remove"})
+     */
+    private $projets;
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Departement", mappedBy="client",cascade={"persist", "remove"})
+     */
+    private $departements;
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Facture", mappedBy="client",cascade={"persist", "remove"})
+     */
+    private $factures;
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Bcclient", mappedBy="client",cascade={"persist", "remove"})
+     */
+    private $bcclients;
+
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Production", mappedBy="client",cascade={"persist", "remove"})
+     */
+    private $productions;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="document_path", fileNameProperty="contratCadre")
+     *
+     * @var File
+     */
+    private $contratCadreFile;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @var string
+     */
+    private $contratCadre;
+
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @Gedmo\Timestampable(on="update")
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $document
+     */
+    public function setContratCadreFile(?File $document = null): void
+    {
+        $this->contratCadreFile = $document;
+
+        if (null !== $document) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getContratCadreFile(): ?File
+    {
+        return $this->contratCadreFile;
+    }
+
+    public function setContratCadre(?string $contratCadre): void
+    {
+        $this->contratCadre = $contratCadre;
+    }
+
+    public function getContratCadre(): ?string
+    {
+        return $this->contratCadre;
+    }
+
+    /**
+     * @var \DateTime
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(name="date_ceation", type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Client
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set tel
+     *
+     * @param string $tel
+     *
+     * @return Client
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    /**
+     * Get tel
+     *
+     * @return string
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * Set fax
+     *
+     * @param string $fax
+     *
+     * @return Client
+     */
+    public function setFax($fax)
+    {
+        $this->fax = $fax;
+
+        return $this;
+    }
+
+    /**
+     * Get fax
+     *
+     * @return string
+     */
+    public function getFax()
+    {
+        return $this->fax;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Client
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     *
+     * @return Client
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->mission = new \Doctrine\Common\Collections\ArrayCollection();
+
+    }
+
+    /**
+     * Add mission
+     *
+     * @param \AppBundle\Entity\Mission $mission
+     *
+     * @return Client
+     */
+    public function addMission(\AppBundle\Entity\Mission $mission)
+    {
+        $this->missions[] = $mission;
+
+        return $this;
+    }
+
+    /**
+     * Remove mission
+     *
+     * @param \AppBundle\Entity\Mission $mission
+     */
+    public function removeMission(\AppBundle\Entity\Mission $mission)
+    {
+        $this->missions->removeElement($mission);
+    }
+
+    /**
+     * Get mission
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getMission()
+    {
+        return $this->missions;
+    }
+
+    /**
+     * Add bcclient
+     *
+     * @param \AppBundle\Entity\Mission $bcclient
+     *
+     * @return Client
+     */
+    public function addBcclient(\AppBundle\Entity\Mission $bcclient)
+    {
+        $this->bcclients[] = $bcclient;
+
+        return $this;
+    }
+
+    /**
+     * Remove bcclient
+     *
+     * @param \AppBundle\Entity\Mission $bcclient
+     */
+    public function removeBcclient(\AppBundle\Entity\Mission $bcclient)
+    {
+        $this->bcclients->removeElement($bcclient);
+    }
+
+    /**
+     * Get bcclient
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getBcclient()
+    {
+        return $this->bcclients;
+    }
+
+    public function __toString()
+    {
+        return $this->getNom();
+    }
+
+    /**
+     * Add departement
+     *
+     * @param \AppBundle\Entity\Departement $departement
+     *
+     * @return Client
+     */
+    public function addDepartement(\AppBundle\Entity\Departement $departement)
+    {
+        $this->departement[] = $departement;
+
+        return $this;
+    }
+
+    /**
+     * Remove departement
+     *
+     * @param \AppBundle\Entity\Departement $departement
+     */
+    public function removeDepartement(\AppBundle\Entity\Departement $departement)
+    {
+        $this->departement->removeElement($departement);
+    }
+
+    /**
+     * Get departement
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getDepartement()
+    {
+        return $this->departement;
+    }
+
+    /**
+     * Add facture
+     *
+     * @param \AppBundle\Entity\Facture $facture
+     *
+     * @return Client
+     */
+    public function addFacture(\AppBundle\Entity\Facture $facture)
+    {
+        $this->factures[] = $facture;
+
+        return $this;
+    }
+
+    /**
+     * Remove facture
+     *
+     * @param \AppBundle\Entity\Facture $facture
+     */
+    public function removeFacture(\AppBundle\Entity\Facture $facture)
+    {
+        $this->factures->removeElement($facture);
+    }
+
+    /**
+     * Get factures
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getFactures()
+    {
+        return $this->factures;
+    }
+
+    /**
+     * Get departements
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getDepartements()
+    {
+        return $this->departements;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Client
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Client
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Get missions
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getMissions()
+    {
+        return $this->missions;
+    }
+
+    /**
+     * Get bcclients
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getBcclients()
+    {
+        return $this->bcclients;
+    }
+
+    /**
+     * Add projet
+     *
+     * @param \AppBundle\Entity\Projet $projet
+     *
+     * @return Client
+     */
+    public function addProjet(\AppBundle\Entity\Projet $projet)
+    {
+        $this->projets[] = $projet;
+
+        return $this;
+    }
+
+    /**
+     * Remove projet
+     *
+     * @param \AppBundle\Entity\Projet $projet
+     */
+    public function removeProjet(\AppBundle\Entity\Projet $projet)
+    {
+        $this->projets->removeElement($projet);
+    }
+
+    /**
+     * Get projets
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getProjets()
+    {
+        return $this->projets;
+    }
+
+    /**
+     * Set ice
+     *
+     * @param string $ice
+     *
+     * @return Client
+     */
+    public function setIce($ice)
+    {
+        $this->ice = $ice;
+
+        return $this;
+    }
+
+    /**
+     * Get ice
+     *
+     * @return string
+     */
+    public function getIce()
+    {
+        return $this->ice;
+    }
+
+    /**
+     * Set echeance
+     *
+     * @param string $echeance
+     *
+     * @return Client
+     */
+    public function setEcheance($echeance)
+    {
+        $this->echeance = $echeance;
+
+        return $this;
+    }
+
+    /**
+     * Get echeance
+     *
+     * @return string
+     */
+    public function getEcheance()
+    {
+        return $this->echeance;
+    }
+
+    /**
+     * Add production
+     *
+     * @param \AppBundle\Entity\Production $production
+     *
+     * @return Client
+     */
+    public function addProduction(\AppBundle\Entity\Production $production)
+    {
+        $this->productions[] = $production;
+
+        return $this;
+    }
+
+    /**
+     * Remove production
+     *
+     * @param \AppBundle\Entity\Production $production
+     */
+    public function removeProduction(\AppBundle\Entity\Production $production)
+    {
+        $this->productions->removeElement($production);
+    }
+
+    /**
+     * Get productions
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getProductions()
+    {
+        return $this->productions;
+    }
+}
